903840a880e9ea1ead99d8292a33ca559c13460b
Bassiro Traore <bachiroufr2002@yahoo.fr>
2019-03-27 09:48:23 +0100

    add circulare graph

--- Projet/CATDBv2/CATdb/modules/graph.py
+++ Projet/CATDBv2/CATdb/modules/graph.py
@@ -20,7 +20,7 @@ except:
     pass
 import  pandas as pd
 import random
-
+import nvd3 as d3
 
 def graph_treatment():    
     requete="SELECT treatment.treatment_type,count(treatment.treatment_type)FROM chips.treatment,chips.experiment "\
@@ -40,16 +40,21 @@ def graph_treatment():
     
     from nvd3 import pieChart
     type = 'pieChart'
-    chart = pieChart(name=type, color_category='category70c', height=400, width=400)
-    #xdata = ["Orange", "Banana", "Pear", "Kiwi", "Apple", "Strawberry", "Pineapple","Orangesdqq", "Bananaqqss", "Peadsr", "Kiwisd", "Applesd", "Strawberrysd", "PineappleS"]
-    #ydata = [3, 4, 6, 1, 5, 7, 3,3, 4, 6, 1, 5, 7, 3]
+    chart = pieChart(name=type, color_category='category20c', height=800, width=800)
     extra_serie = {"tooltip": {"y_start": "", "y_end": " cal"}}
     chart.add_serie(y=ydata, x=xdata, extra=extra_serie)
     chart.buildcontent()
     text=chart.htmlcontent
     
-    text=text.replace('chart.tooltipContent','/*chart.tooltipContent');
-    text=text.replace('return tooltip_str;\n              });\n       ','return tooltip_str;\n              });*\ \n       ')
+    
+    argument1="""chart.color(d3.scale.category20c().range());\n\n    chart.tooltipContent(function(key, y, e, graph) {\n          var x = String(key);\n              var y =  String(y)  + \' cal\';\n\n              tooltip_str = \'<center><b>\'+x+\'</b></center>\' + y;\n              return tooltip_str;\n              });\n"""
+    argument2=""
+    
+    
+    text=text.replace(argument1,argument2);
+    #text=text.replace('return tooltip_str;\n              });\n       ','return tooltip_str;\n              });*\ \n       ')
     
     return text
-    
+
+#text=graph_treatment()
+#print(text)
